a3360b385070f2b115834023edf44c5eba87333a
scarfacedotcom <peteradaaku@gmail.com>
2023-03-24 06:25:05 +0100

    refactor code

--- test/Purchase.t.sol
+++ test/Purchase.t.sol
@@ -1,40 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-
-import "forge-std/Test.sol";
+import "lib/forge-std/src/Test.sol";
 import "../src/AssetPurchase.sol";
+import "../src/SFN.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
-import "../interfaces/AggregatorV3Interface.sol";
-import "../src/AssetPurchase.sol";
+contract AssetMarket is Test {
+    Market public market;
+    NFT public nft;
+    IERC20 public daiContract;
+function setUp() public {
+    vm.startPrank(0xBB9F947cB5b21292DE59EFB0b1e158e90859dddb);
+    uint mainnet = vm.createFork("https://eth-mainnet.g.alchemy.com/v2/xypdsCZYrlk6oNi93UmpUzKE9kmxHy2n", 16890887);
+    vm.selectFork(mainnet);
+    nft = new NFT();
+    market = new Market(address(nft));
+    daiContract = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    vm.stopPrank();
+}
 
-contract AssetPurchaseTest is Test {
-    
+function testMint() public {
+    vm.startPrank(0xBB9F947cB5b21292DE59EFB0b1e158e90859dddb);
+    nft.safeMint("https://ipfs.filebase.io/ipfs/QmYqEcCNJiP7pP2nzSsvyv7Ji1tNpv6omWMJ4Nph22dmfn", address(market));
+    market.listItem(0);
+    vm.stopPrank();
+}
+function testPurchaseItem() public {
+    testMint(); 
+    vm.startPrank(0x748dE14197922c4Ae258c7939C7739f3ff1db573);
+    daiContract.approve(address(market), 35000000000000000000000000000000000000000000000000000);
+    market.purchaseItem(0);
+    uint balance = daiContract.balanceOf(address(market));
+    console.log(balance);
+}
 
-    AssetPurchase public assetPurchase;
-    
-    AggregatorV3Interface public priceFeed;
-
-    function setUp() public {
-        // Deploy a new instance of AssetPurchase
-        address priceFeedAddress = 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e; 
-        
-        assetPurchase = new AssetPurchase(priceFeedAddress);
-        priceFeed = AggregatorV3Interface(priceFeedAddress);
-
-        // Set the asset price to 3.5 ether
-        assetPurchase.assetPrice().set(3.5 ether);
-    }
-
-    function testPurchaseAsset() public {
-        
-        int256 price = assetPurchase.getLastestPrice();
-        price.expect().to.be.above(0);
-
-
-        uint256 usdtValue = uint256(price) * uint256(assetPurchase.assetPrice()) / 10 ** 18;
-
-
-        // uint256 ethValue = usdtValue * 10 ** 10; // Multiply by 10^10 because 1 USDT = 10^10 Wei
-        // expect(() => assetPurchase.purchaseAsset{ value: ethValue }()).to.change(() => assetPurchase.purchased()).from(false).to(true);
-    }
 }
